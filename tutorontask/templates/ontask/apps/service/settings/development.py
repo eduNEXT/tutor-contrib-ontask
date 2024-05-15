942baa8c91ac4f3e3acf0d8a4c9036d1f2fe8e8b
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-05-15 10:44:32 -0400

    refactor: improve logging configurations and use latest ontask release

--- tutorontask/templates/ontask/apps/service/settings/development.py
+++ tutorontask/templates/ontask/apps/service/settings/development.py
@@ -1,35 +1,8 @@
-"""Configuration for production."""
+"""Configuration for development."""
 import logging.config
 
 from settings.base import *  # NOQA
 
-# Security features
-# -----------------------------------------------------------------------------
-MIDDLEWARE += ['django.middleware.security.SecurityMiddleware']
-if USE_SSL:
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_SSL_REDIRECT = True
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-else:
-    SECURE_CONTENT_TYPE_NOSNIFF = False
-    SECURE_BROWSER_XSS_FILTER = False
-    SESSION_COOKIE_SECURE = False
-    CSRF_COOKIE_SECURE = False
-    SECURE_SSL_REDIRECT = False
-
-# Cache the templates in memory for speed-up
-loaders = [
-    ('django.template.loaders.cached.Loader', [
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader'])]
-
-TEMPLATES[0]['OPTIONS'].update({"loaders": loaders})
-TEMPLATES[0].update({"APP_DIRS": False})
 
 def get_logger_config(
     debug=False,
@@ -98,6 +71,6 @@ def get_logger_config(
     return logger_config
 
 
-logging.config.dictConfig(get_logger_config())
+logging.config.dictConfig(get_logger_config(debug=DEBUG))
 
-{{ ONTASK_EXTRA_PRODUCTION_SETTINGS }}
+{{ ONTASK_EXTRA_DEV_SETTINGS }}
